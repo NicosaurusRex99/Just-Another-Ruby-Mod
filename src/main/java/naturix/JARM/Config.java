eec371e60a7c883bfd9bbf7271f3681424e8dacf
NicosaurusRex99 <pusspot1999@gmail.com>
2017-09-25 15:25:55 +0930

    Made Ruby Sword Damage Configurable

--- src/main/java/naturix/JARM/Config.java
+++ src/main/java/naturix/JARM/Config.java
@@ -12,8 +12,8 @@ public class Config {
 
     private static final String CATEGORY_GENERAL = "general";
 
-    public static boolean AddMeme = true;
     public static boolean isRubyBeaconBase = true;
+    public static int rubyDamage = 9;
 
     // Call this from CommonProxy.preInit(). It will create our config if it doesn't
     // exist yet and read the values if it does exist.
@@ -34,8 +34,9 @@ public class Config {
     private static void initGeneralConfig(Configuration cfg) {
         cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General configuration");
         // cfg.getBoolean() will get the value in the config if it is already specified there. If not it will create the value.
-        AddMeme = cfg.getBoolean("addMeme", CATEGORY_GENERAL, AddMeme, "Set to false if you don't like memes");
         isRubyBeaconBase = cfg.getBoolean("isRubyBeaconBase", CATEGORY_GENERAL, isRubyBeaconBase, "Set to false to disable ruby block from being a beacon base");
+        rubyDamage = cfg.getInt("rubyDamage", CATEGORY_GENERAL, 9, 0, 2400000, "choose a number between 0 and 2400000to change the damage on the ruby sword");
     }
+    
 
 }
