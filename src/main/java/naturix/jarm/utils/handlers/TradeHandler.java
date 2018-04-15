e2dd00aeee2e05a2b85aeea27adb2a3ebff93a82
NicosaurusRex99 <pusspot1999@gmail.com>
2018-04-15 19:14:48 +0930

    Began adding trades
    Hopefully releasing 1.12.2.13 in 2 days

--- src/main/java/naturix/jarm/utils/handlers/TradeHandler.java
+++ src/main/java/naturix/jarm/utils/handlers/TradeHandler.java
@@ -3,6 +3,8 @@ package naturix.jarm.utils.handlers;
 import java.util.Random;
 
 import naturix.jarm.registry.ModBlocks;
+import naturix.jarm.registry.ModItems;
+import naturix.jarm.utils.config.ConfigMain;
 import net.minecraft.entity.IMerchant;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.init.Items;
@@ -15,6 +17,35 @@ public class TradeHandler implements EntityVillager.ITradeList
    @Override
    public void addMerchantRecipe(IMerchant merchant, MerchantRecipeList recipeList, Random random)
    {
+	   if(ConfigMain.amethystModule == true) {
       recipeList.add(new MerchantRecipe(new ItemStack(Items.EMERALD, 10 + random.nextInt(15)),new ItemStack(ModBlocks.block_amethyst,1 + random.nextInt(2))));
+   }
+	   if(ConfigMain.rubyModule == true) {
+		   recipeList.add(new MerchantRecipe(new ItemStack(Items.REDSTONE, 5 + random.nextInt(20)),new ItemStack(ModItems.gemRuby, 1+ random.nextInt(8))));
+	   }
+	   
+	   /*
+	   gemBraunite
+	   gemAmber
+	   gemCinnibar
+	   gemFossil
+	   gemJade
+	   gemMeteorite
+	   gemOpal
+	   gemSaphire
+	   ingot_tin
+	   ingot_copper
+	   ingot_lead
+	   ingot_silver
+	   ingot_tungsten
+	   ingot_platinum
+	   ingot_cobalt
+	   ingot_mythrill
+	   ingot_titanium
+	   ingot_luminite
+	   ingot_uranium
+	   ingot_nickel
+	   ingot_steel
+	   ingot_osmium*/
    }
 }
