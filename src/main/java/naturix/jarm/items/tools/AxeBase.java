19d7981d1bda7bde0a239efe1189550a0266b582
NicosaurusRex99 <pusspot1999@gmail.com>
2018-05-11 15:01:14 +0930

    fixed axe

--- src/main/java/naturix/jarm/items/tools/AxeBase.java
+++ src/main/java/naturix/jarm/items/tools/AxeBase.java
@@ -9,12 +9,12 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemTool;
 
-public class AxeBase extends ItemTool{
+public class AxeBase extends net.minecraft.item.ItemAxe{
 	private static final Set<Block> EFFECTIVE_ON = Sets.newHashSet(Blocks.PLANKS, Blocks.COCOA, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE);
     
 public String name;
 public AxeBase(ToolMaterial material, String name) {
-	super(11, 4, material, EFFECTIVE_ON);
+	super(material, 11, 3);
     setRegistryName(name);
     setUnlocalizedName(JARM.MODID + "." + name);
     setCreativeTab(JARM.JARM);
