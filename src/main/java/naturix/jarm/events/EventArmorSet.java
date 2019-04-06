76159dbe63c99cde01a9dfa6f486f3eabc704c21
NicosaurusRex99 <pusspot1999@gmail.com>
2019-04-06 15:14:33 +1030

    updates

--- src/main/java/naturix/jarm/events/EventArmorSet.java
+++ src/main/java/naturix/jarm/events/EventArmorSet.java
@@ -1,11 +1,12 @@
 package naturix.jarm.events;
 
 import naturix.jarm.registry.ModItems;
-import net.minecraft.init.MobEffects;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.DamageSource;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,10 +17,8 @@ public class EventArmorSet{
 
 	private ModItems item = new ModItems();
 	private Item boots = null, body = null, legs = null, helmet = null;
-	boolean isDone, isDone2, isDone3;
 	@SubscribeEvent
 	public void playerTick(PlayerTickEvent event) {
-		//Literally the same thing that armorItemInSlot does, but not set as client-sided. WTF Forge.
 		ItemStack stackBoots = event.player.inventory.armorInventory.get(0);
 		ItemStack stackLegs = event.player.inventory.armorInventory.get(1);
 		ItemStack stackBody = event.player.inventory.armorInventory.get(2);
@@ -32,46 +31,23 @@ public class EventArmorSet{
 		else legs = null;
 		if(stackHelmet != null) helmet = stackHelmet.getItem();
 		else helmet = null;
-
+		
 		if(helmet == 
 				ModItems.helmetAmethyst && body == 
 				ModItems.chestplateAmethyst && legs ==
 				ModItems.leggingsAmethyst && boots == 
-				ModItems.bootsAmethyst) 
+				ModItems.bootsAmethyst && event.player.capabilities.allowFlying == false)
 		{
 		event.player.fallDistance = 0;
-		event.player.capabilities.allowFlying = true;
-		isDone=false;
-		}else {
-			if(event.player.isCreative() != true && event.player.capabilities.allowFlying || event.player.capabilities.isFlying
-					|| helmet == ModItems.helmetAmethyst || body == ModItems.chestplateAmethyst || legs == ModItems.leggingsAmethyst || boots == ModItems.bootsAmethyst
-					) {
-				if(isDone==false) {
-				event.player.capabilities.isFlying = false;
-				event.player.capabilities.allowFlying = false;
-				isDone=true;
-				}
 		}
-		}
+		
 		if(helmet == 
 				ModItems.helmetBraunite&& body == 
 				ModItems.chestplateBraunite && legs ==
 				ModItems.leggingsBraunite && boots == 
-				ModItems.bootsBraunite) 
+				ModItems.bootsBraunite && event.player.capabilities.allowFlying == false)
 		{
 		event.player.fallDistance = 0;
-		event.player.capabilities.allowFlying = true;
-		isDone2=false;
-		}else {
-			if(event.player.isCreative() != true && event.player.capabilities.allowFlying || event.player.capabilities.isFlying
-					|| helmet == ModItems.helmetBraunite || body == ModItems.chestplateBraunite || legs == ModItems.leggingsBraunite || boots == ModItems.bootsBraunite
-					) {
-				if(isDone2==false) {
-				event.player.capabilities.isFlying = false;
-				event.player.capabilities.allowFlying = false;
-				isDone2=true;
-				}
-		}
 		}
 //		
 		if(helmet == ModItems.helmetOpal) {
@@ -87,21 +63,9 @@ public class EventArmorSet{
 				ModItems.leggingsOpal && boots == 
 				ModItems.bootsOpal) 
 		{
-		event.player.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("invisibility"), 2, 5));
-		isDone3=false;
-		}else {
-			if(event.player.isCreative() != true && event.player.capabilities.allowFlying || event.player.capabilities.isFlying
-					|| helmet == ModItems.helmetOpal || body == ModItems.chestplateOpal || legs == ModItems.leggingsOpal || boots == ModItems.bootsOpal
-					) {
-				if(isDone3==false) {
-				event.player.capabilities.isFlying = false;
-				event.player.capabilities.allowFlying = false;
-				isDone3=true;
-				}
-		}
-		}
-		if(event.player.isCreative() == true) {
-			event.player.capabilities.allowFlying = true;
+		
+			event.player.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation("invisibility"), 2, 5));
+
 		}
 	}
 	
@@ -112,8 +76,31 @@ public class EventArmorSet{
 		}
 
 		@SubscribeEvent
-		public void onLivingHurtEvent(LivingHurtEvent e) {
+		public void onLivingHurtEvent(LivingHurtEvent event) {
+			EntityPlayer player = (EntityPlayer)event.getEntity();
+			ItemStack stackBoots = player.inventory.armorInventory.get(0);
+			ItemStack stackLegs = player.inventory.armorInventory.get(1);
+			ItemStack stackBody = player.inventory.armorInventory.get(2);
+			ItemStack stackHelmet = player.inventory.armorInventory.get(3);
+			if(stackBoots != null) boots = stackBoots.getItem();
+			else boots = null;
+			if(stackBody != null) body = stackBody.getItem();
+			else body = null;
+			if(stackLegs != null) legs = stackLegs.getItem();
+			else legs = null;
+			if(stackHelmet != null) helmet = stackHelmet.getItem();
+			else helmet = null;
 			
+			if(helmet == 
+					ModItems.helmetRuby && body == 
+					ModItems.chestplateRuby && legs ==
+					ModItems.leggingsRuby && boots == 
+					ModItems.bootsRuby)
+			{
+				if(event.getSource() == DamageSource.GENERIC) {
+					event.setCanceled(true);
+				}
+			}
 		}
 
 }
