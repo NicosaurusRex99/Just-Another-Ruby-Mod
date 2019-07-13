7b2c491e7c76ca004c91ab6b1193309d4f2b5aeb
NicosaurusRex99 <pusspot1999@gmail.com>
2019-07-13 13:19:25 +0930

    fixed block bounds

--- src/main/java/naturix/ruby/objects/blocks/Amethyst.java
+++ src/main/java/naturix/ruby/objects/blocks/Amethyst.java
@@ -1,13 +1,27 @@
 package naturix.ruby.objects.blocks;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 
 public class Amethyst extends BlockBase {
     public Amethyst(String name, int lvl){
         super(name, lvl);
     }
 
+
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.CUTOUT;
+    }
+
+    protected static final VoxelShape AMETHYSTSHAPE = Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 4.0D, 12.0D);
+
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
+    {
+        return AMETHYSTSHAPE;
     }
 }
--- src/main/java/naturix/ruby/objects/blocks/Meteorite.java
+++ src/main/java/naturix/ruby/objects/blocks/Meteorite.java
@@ -2,11 +2,17 @@ package naturix.ruby.objects.blocks;
 
 import naturix.ruby.registry.ModBlocks;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.FallingBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 
 public class Meteorite extends FallingBlock {
+
     public Meteorite(String name) {
         super(Block.Properties
                 .create(Material.ROCK)
@@ -16,8 +22,14 @@ public class Meteorite extends FallingBlock {
 
         ModBlocks.BLOCKS.add(this);
     }
+    protected static final VoxelShape METEORESHAPE = Block.makeCuboidShape(1D, 0.0D, 1D, 14D, 10D, 14D);
 
     public BlockRenderLayer getRenderLayer() {
         return BlockRenderLayer.CUTOUT;
+    }
+
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
+    {
+        return METEORESHAPE;
     }
 }
